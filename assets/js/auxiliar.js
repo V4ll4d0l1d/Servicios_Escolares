a36e5049d57ea670484c21930641193e9eef5236
sarahdark77 <sbobadillaperez@gmail.com>
2021-02-25 14:56:12 -0600

    Update auxiliar.js

--- assets/js/auxiliar.js
+++ assets/js/auxiliar.js
@@ -73,3 +73,12 @@ function showGrupos() {
     xmlhttp.send();
   }
 }
+
+function responsiveMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
