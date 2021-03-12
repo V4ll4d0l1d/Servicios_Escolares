c4172fc1f3489fe673d119cc3102386a67ae427a
Jesús <jjmoreno23@gmail.com>
2021-03-12 11:16:47 -0600

    SQL para añadir los usuarios de prueba

---
+++ DB/subirusuarios.sql
@@ -0,0 +1,97 @@
+﻿/* INSERT QUERY NO: 1 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'bachillerato1', 'Titular Bach 1ro', 3, 1, 'NO'
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'bachillerato3', 'Titular Bach 3ro', 3, 3, 'NO'
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'fisioterapia1', 'Titular LFR 1ro', 4, 1, 'LFR'
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'fisioterapia3', 'Titular LFR 3ro', 4, 3, 'LFR'
+);
+
+/* INSERT QUERY NO: 5 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'preescolar1A', 'Titular PRE11', 0, 1, 'NO'
+);
+
+/* INSERT QUERY NO: 6 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'preescolar2A', 'Titular PRE21', 0, 2, 'NO'
+);
+
+/* INSERT QUERY NO: 7 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'primaria1A', 'Titular PRI11', 1, 2, 'NO'
+);
+
+/* INSERT QUERY NO: 8 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'primaria2A', 'Titular PRI21', 1, 2, 'NO'
+);
+
+/* INSERT QUERY NO: 9 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'secundaria1A', 'Titular SEC11', 2, 2, 'NO'
+);
+
+/* INSERT QUERY NO: 10 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'secundaria2A', 'Titular SEC21', 2, 2, 'NO'
+);
+
+/* INSERT QUERY NO: 1 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'controlprimaria', 'Control Escolar Primaria', 1, '', 'NO'
+);
+
+/* INSERT QUERY NO: 2 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'controlsecundaria', 'Control Escolar Secundaria', 2, '', 'NO'
+);
+
+/* INSERT QUERY NO: 3 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'controlpreescolar', 'Control Escolar Preescolar',  0, '', 'NO'
+);
+
+/* INSERT QUERY NO: 4 */
+INSERT INTO datosidusuario(Id, Nombres, Seccion, Grado, Carrera)
+VALUES
+(
+'controlbachillerato', 'Control Escolar bachillerato', 3, '', 'NO'
+);
---
+++ DB/usuarios.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO `usuarios` (`Id`, `Pass`, `Type`, `Privileges`) VALUES
+('bachillerato1', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('bachillerato3', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('fisioterapia1', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('fisioterapia3', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('preescolar1A', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('preescolar2A', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('primaria1A', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('primaria2A', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('secundaria1A', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('secundaria2A', 'e10adc3949ba59abbe56e057f20f883e', 1, 2),
+('controlprimaria', 'e10adc3949ba59abbe56e057f20f883e', 1, 3),
+('controlsecundaria', 'e10adc3949ba59abbe56e057f20f883e', 1, 3),
+('controlpreescolar', 'e10adc3949ba59abbe56e057f20f883e', 1, 3),
+('controlbachillerato', 'e10adc3949ba59abbe56e057f20f883e', 1, 3);
